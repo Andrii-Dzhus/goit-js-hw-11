4ead5fecf300475836b00be5cfa6ee21a6fc4a83
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-07-07 10:12:10 +0300

    07/07/24

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,23 +14,23 @@ export function renderGallery(images) {
         comments,
         downloads,
       }) => `
-      <a href="${largeImageURL}">
         <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          </a>
           <div class="info">
-            <p class="info-item"><b>Likes</b>${likes}</p>
-            <p class="info-item"><b>Views</b>${views}</p>
-            <p class="info-item"><b>Comments</b>${comments}</p>
-            <p class="info-item"><b>Downloads</b>${downloads}</p>
+            <p class="info-item"><b>Likes</b> ${likes}</p>
+            <p class="info-item"><b>Views</b> ${views}</p>
+            <p class="info-item"><b>Comments</b> ${comments}</p>
+            <p class="info-item"><b>Downloads</b> ${downloads}</p>
           </div>
         </div>
-      </a>
     `
     )
     .join('');
   gallery.innerHTML = markup;
 
-  new SimpleLightbox('.gallery a', {
+  new SimpleLightbox('.photo-card a', {
     captionsData: 'alt',
     captionDelay: 250,
   }).refresh();
